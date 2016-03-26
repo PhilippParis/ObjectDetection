c6d844b6aed84fdd41a2aa86f614474ac5a3618b
Philipp Paris <paris.philipp@gmail.com>
2016-03-26 18:16:33 +0100

    added documentation, renamed variables and reworked output

--- object_detection.py
+++ object_detection.py
@@ -5,6 +5,7 @@ import utils
 import numpy
 import cv2
 import time
+import csv
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -13,6 +14,12 @@ flags.DEFINE_integer('image_size', 28, 'width and height of the input images')
 flags.DEFINE_integer('batch_size', 50, 'training batch size')
 flags.DEFINE_integer('max_steps', 1000, 'number of steps to run trainer')
 
+flags.DEFINE_string('test_img', '../space_crater_dataset/images/tile3_25.pgm', 'path to test image')
+flags.DEFINE_string('test_data', '../space_crater_dataset/data/3_25.csv', 'path to ground truth csv file')
+flags.DEFINE_string('output_file','output/3_25_sliding_window_out', 'path to output file')
+
+flags.DEFINE_integer('step_size', 7, 'sliding window step size')
+
 # start session
 sess = tf.InteractiveSession()
 
@@ -20,7 +27,6 @@ def import_data():
     """
     Returns a Data object which contains training data
     """
-    
     # Import data
     data = input_data.Data(FLAGS.image_size, FLAGS.image_size)
     #data.add('../images/Land.csv',
@@ -65,9 +71,18 @@ def overall_accuracy(data, x, y_, keep_prob, acc_op):
 
 
 def train_model(model, data, x, y_, keep_prob):
+    """
+    trains the model
+    Args:
+        model: model to train
+        data: training datasets
+        x: input data placeholder
+        y_: desired output placeholder
+        keep_prob: keep probability placeholder    
+    """
     global sess
     with tf.name_scope('test'):
-        correct_prediction = tf.equal(tf.argmax(desired_output, 1), tf.argmax(model, 1))
+        correct_prediction = tf.equal(tf.argmax(y_, 1), tf.argmax(model, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         _ = tf.scalar_summary('accuracy', accuracy)
     
@@ -104,24 +119,37 @@ def train_model(model, data, x, y_, keep_prob):
     
     
 
-def detect_objects(model, x, keep_prob):
+def sliding_window_detection(model, x, keep_prob, src):
+    """
+    object detection via sliding windows
+    Args:
+        model: model which is used for detection
+        x: input data placeholder
+        keep_prob: keep probability placeholder
+        src: image to apply the detection
+    Returns:
+        list of found craters [(x,y,radius)]
+    
+    """
     global sess
-    src = utils.getImage('../space_crater_dataset/images/tile3_25.pgm')
-    for windows, coords in utils.slidingWindow(src, 7, (FLAGS.image_size, FLAGS.image_size)):
+    objects = []
+    for windows, coords in utils.slidingWindow(src, FLAGS.step_size, (FLAGS.image_size, FLAGS.image_size)):
         
         feed = {x:windows, keep_prob:1.0}
         y = sess.run(model, feed_dict = feed)
 
         for i in range(0, len(y)):
             if y[i][0] < 0.25 and y[i][1] > 0.75:
-                cv2.circle(src,(coords[i][0], coords[i][1]), 2, (0,0,255), 0)
-    return src        
+                objects.append((coords[i][0], coords[i][1], 2))
+                #cv2.circle(src,(coords[i][0], coords[i][1]), 2, (0,0,255), 0)
+    return objects        
     
 def main(_):
     # import data
     data = import_data()
 
     # ---------- create model ----------------#
+    
     # model input placeholder
     x           = tf.placeholder("float", shape=[None, FLAGS.image_size * FLAGS.image_size])
     # desired output placeholder
@@ -137,17 +165,34 @@ def main(_):
     tf.initialize_all_variables().run()
 
     # ---------- train model -----------------#
+    
     start = time.time()
     train_model(model, data, x, y_, keep_prob)
     print 'training time: %d' % (time.time() - start)
     
     # ---------- object detection ------------#
+    
+    src = utils.getImage(FLAGS.test_img)
     start = time.time()
-    detected = detect_objects(model, x, keep_prob)
+    objects = sliding_window_detection(model, x, keep_prob, src)
     print 'detection time: %d' % (time.time() - start)
     
-    utils.showImage('title', detected)
     
+    # ----------- output ---------------------#
+    
+    src = cv2.cvtColor(src,cv2.COLOR_GRAY2RGB)
+    # mark ground truth craters
+    ground_truth_data = open(data_path, 'rb')
+    for row in csv.reader(ground_truth_data, delimiter=','):
+        if int(row[3]) == 1:
+            cv2.circle(src,int(row[0]), int(row[1]), int(row[2]), (0,255,0), 0)
+    
+    # mark found objects
+    for (x,y,r) in objects:
+                cv2.circle(src,(x, y), r, (255,0,0), 0)
+    
+    utils.showImage('title', detected)
+    cv2.imwrite(FLAGS.output_file, src)
 
 if __name__ == '__main__':
     tf.app.run()
